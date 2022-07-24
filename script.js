67899eac25b9051e6cd6da663dd3b3385708a371
Ali <ali-alsadah1941@hotmail.com>
2022-07-24 17:36:57 +0300

    create the initial board

---
+++ script.js
@@ -0,0 +1,45 @@
+//factory function (module)
+const gameBoard = () => {
+    let gameBoardArray = new Array(3).fill(new Array(3));
+
+    //start the game
+    const startGame = (enemie) => {
+
+    }
+    
+    //restart the gameBoard
+    const resetGame = () => {
+
+    }
+
+    //there is possibility of a tie
+    const declareWinner = (player1, player2) => {
+
+    }
+
+}
+
+
+
+
+//factory function
+const player = (name) => {
+    const marker = "";
+
+    //The player will have the chance to play against computer or a real player
+    const playAgainst = (enemie) => {
+
+    }
+
+    //the player will have the chance to choose X or O
+    const chooseMark = (marker) => {
+
+    }
+
+}
+
+
+// const player1 = new player("ali");
+// const player2 = new player("ali2");
+
+// gameBoard().declareWinner(player1,player2);
